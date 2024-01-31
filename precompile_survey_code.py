fbfee8caf26cc4daaf2d5ac8ef065b980463533e
Johan Henkens <johan@henkens.com>
2024-01-31 12:51:27 -0800

    Switch to templating with Jinja

--- precompile_survey_code.py
+++ precompile_survey_code.py
@@ -4,19 +4,38 @@ import jinja2
 import pathlib
 
 
-CURRENT_FILE=pathlib.Path(__file__)
+CURRENT_FILE = pathlib.Path(__file__)
 BASE_DIR = CURRENT_FILE.parent
-YAML_FILE=BASE_DIR / "surveys.yaml"
+TEMPLATES_DIR = BASE_DIR / "Templates"
+YAML_FILE = TEMPLATES_DIR / "surveys.yaml"
+TEMPLATE_FILE = TEMPLATES_DIR / "Survey.CRB"
 
-extensions = {
-    "cr300": "CR300",
-    "cr1000": "CR1",
-    "cr6": "CR6"
-}
+extensions = {"CR300": "CR300", "CR1000": "CR1", "CR6": "CR6"}
 if __name__ == "__main__":
-    with open(YAML_FILE,'r') as yaml_file_raw:
-        survey_config = yaml.load(yaml_file_raw)
-    for station in survey_config.get("stations",None):
-        file_name = station["survey_name"]
-        extension = extensions[station["model_series"]]
-        dest_file = BASE_DIR / f"{file_name}.{extension}"
+    with open(TEMPLATE_FILE, "r") as template_file_raw:
+        template_file = template_file_raw.read()
+
+    with open(YAML_FILE, "r") as yaml_file_raw:
+        survey_config = yaml.safe_load(yaml_file_raw.read())
+
+    shared_config = survey_config.get("shared", None) or {}
+    for station in survey_config.get("stations", None):
+        model_series = station["model_series"]
+
+        model_config = survey_config.get(model_series, None) or {}
+        station_config = {**shared_config, **model_config, **station}
+
+        file_name = station_config["station_name"]
+        extension = extensions[model_series]
+
+        dest_file_name = BASE_DIR / f"{file_name}.{extension}"
+
+        template = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(TEMPLATES_DIR),
+            lstrip_blocks=True,
+            trim_blocks=True,
+        ).from_string(template_file)
+        content = template.render(**station_config)
+
+        with open(dest_file_name, "w") as dest_file:
+            dest_file.write(content)
